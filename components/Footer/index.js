4975cc1bbb0b4af3abc56ba3fc09c91fa6b67970
Victor de Oliveira <59441152+oliveira-vh@users.noreply.github.com>
2020-12-21 10:54:33 -0300

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='bg-gray-700 p-4'>
             <div className='container mx-auto  text-center font-bold text-white'>
-            Projeto desenvolvido por Victor de Oliveira / {' '}
+            Projeto desenvolvido por Victor de Oliveira no treinamento FullStack Master - DevPleno / {' '}
             <a className='hover:underline' href='https://github.com/oliveira-vh' target="_blank">Github</a> / {' '}
             <a className='hover:underline' href='https://www.linkedin.com/in/oliveira-vh/' target="_blank">Linkedin</a>
             </div>
